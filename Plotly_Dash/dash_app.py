15ca02f4eedb41eab37f760f63fb85661bc6593f
Divya Ravali <ssdivyaravali@gmail.com>
2022-09-15 15:46:06 +0530

    add dash code

---
+++ Plotly_Dash/dash_app.py
@@ -0,0 +1,53 @@
+# import the libraries
+import pandas as pd
+import dash 
+import dash_html_components as html
+#from dash import html
+import dash_core_components as dcc
+#from dash import dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+from top_directors import imdb_movies_cleaned_income_explode_director,directors
+
+# Create the Dash app
+app = dash.Dash()
+
+# Layout section of dashboard basically how the app would look like
+app.layout = html.Div(children=[
+    html.H1(children='Imdb score and Gross Income  Dashboard'),
+    html.H2(children='Director'),
+    dcc.Dropdown(
+        id = 'Director-dropdown',
+        options=[{'label':i,'value':i} for i in directors],
+        value='All Top 10 Directors'
+    ),dcc.Graph(id='income-imbdscore-graph')
+]
+)
+# add interactivity to the code - basically create a scatter plot
+# we will filter out movies based on their director 
+# then for those movies we will make scatter plot of imdb_score and worldwide gross income
+# Set up the callback function
+@app.callback(
+    Output(component_id='income-imbdscore-graph', component_property='figure'),
+    [
+     Input(component_id='Director-dropdown',component_property='value')
+    ]
+)
+def update_graph(directors):    
+    if directors == 'All Top 10 Directors':
+      director_select = ''
+    else:
+      director_select = directors
+    
+    filtered_final = imdb_movies_cleaned_income_explode_director.loc[imdb_movies_cleaned_income_explode_director['director_list'].str.contains(director_select,na=False)]
+    print(filtered_final.shape)
+    
+    scatter_fig = px.scatter(filtered_final,
+                       x='worldwide_gross_income', y='imdb_score',hover_name='original_title',
+                       hover_data=['director_list'],
+                       title='Ratings and worldwide_gross_income for movies for director {}'.format(directors))
+    return scatter_fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ Plotly_Dash/top_directors.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+imdb_movies = pd.read_csv('F:/Practice/PRACTICE/Plotly_Dash/IMDb movies.csv')
+imdb_movies.drop(['title','reviews_from_users','reviews_from_critics','writer','production_company','description','usa_gross_income','metascore'],axis=1,inplace=True)
+imdb_movies.rename({'worlwide_gross_income':'worldwide_gross_income','avg_vote':'imdb_score'},axis=1,inplace=True)
+
+imdb_movies_cleaned = imdb_movies.loc[imdb_movies['worldwide_gross_income'].notnull()].copy()
+imdb_movies_cleaned['year'] = imdb_movies_cleaned['year'].astype(int)
+
+imdb_movies_cleaned_income =  imdb_movies_cleaned.loc[\
+                                imdb_movies_cleaned['worldwide_gross_income'].str.startswith('$')].copy()
+def remove_dollar_sign(gross_income):
+    return gross_income.replace('$','')
+imdb_movies_cleaned_income['worldwide_gross_income_numbered'] = imdb_movies_cleaned_income.apply(lambda \
+                                                    row: remove_dollar_sign(row['worldwide_gross_income']),axis=1)
+imdb_movies_cleaned_income['worldwide_gross_income_numbered'] = imdb_movies_cleaned_income[\
+                                                              'worldwide_gross_income_numbered'].astype(float)
+imdb_movies_cleaned_income.drop(['worldwide_gross_income'],axis=1,inplace=True)
+imdb_movies_cleaned_income.rename({'worldwide_gross_income_numbered':'worldwide_gross_income'},axis=1,inplace=True)
+imdb_movies_cleaned_income['worldwide_gross_income'] = imdb_movies_cleaned_income['worldwide_gross_income']/1000000
+
+imdb_movies_cleaned_income['director'] = imdb_movies_cleaned_income['director'].fillna('No Director')
+imdb_movies_cleaned_income['actors'] = imdb_movies_cleaned_income['actors'].fillna('No Actor')
+
+def convert_col_list(col):
+  split_col = col.split(',')
+  remove_spaces_col_list = [x.strip() for x in split_col]
+  return remove_spaces_col_list
+
+imdb_movies_cleaned_income['director_list'] = imdb_movies_cleaned_income.apply(lambda row:convert_col_list(row['director']),axis=1)
+imdb_movies_cleaned_income_explode_director = imdb_movies_cleaned_income.explode('director_list')
+
+movies_director = imdb_movies_cleaned_income_explode_director.groupby('director_list')['imdb_title_id'].agg('count').rename('no_of_movies').reset_index()
+movies_director.sort_values(by='no_of_movies',ascending=False,inplace=True)
+
+directors = list(movies_director[:10]['director_list'])+ ['All Top 10 Directors']
+#print(directors)
